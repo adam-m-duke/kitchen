dadb1c7628315813c7b27835dadc82348d8e53f7
Adam Duke <adamduke@uchicago.edu>
2024-07-13 18:17:33 +0000

    reorganizaiton

---
+++ src/appetizer/main.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+#include <absl/container/flat_hash_map.h>
+#include <benchmark/benchmark.h>
+#include <boost/asio.hpp>
+#include <Eigen/Dense>
+#include <flatbuffers/flatbuffers.h>
+#include <gtest/gtest.h>
+#include <hwy/highway.h>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
---
+++ src/appetizer/main.h
@@ -0,0 +1,4 @@
+#ifndef APPETIZER__MAIN_H
+#define APPETIZER__MAIN_H
+
+#endif
